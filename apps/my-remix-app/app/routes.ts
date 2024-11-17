8c9a8941cab6b4466c071f93e74ad12fcf81ddce
jrestall <james.restall@gmail.com>
2024-11-17 13:54:34 +1300

    fix: update routes.ts

--- apps/my-remix-app/app/routes.ts
+++ apps/my-remix-app/app/routes.ts
@@ -1,5 +1,3 @@
-import { relative } from 'node:path';
-import { existsSync } from 'node:fs';
 import { flatRoutes } from '@react-router/fs-routes';
 import { type RouteConfig } from '@react-router/dev/routes';
 import { createGlobPatternsForDependencies } from '@nrwl/react/tailwind';
@@ -7,12 +5,7 @@ import { createGlobPatternsForDependencies } from '@nrwl/react/tailwind';
 const routeConfigs = createGlobPatternsForDependencies(
   __dirname,
   '/src/routes/',
-)
-  .map((dir) => relative(__dirname, dir))
-  .map((dir) => {
-    console.log(dir);
-    return flatRoutes({ rootDirectory: dir });
-  });
+).map((rootDirectory) => flatRoutes({ rootDirectory }));
 
 const resolvedRoutes = [
   ...(await flatRoutes()),
@@ -21,14 +14,11 @@ const resolvedRoutes = [
 
 console.log(JSON.stringify(resolvedRoutes, null, 2));
 
-// Fix the 'file' property of the routes since it is cut off with:
+// Fix the 'file' property of the package routes since they are cut off with:
 // file: file.slice(appDirectory.length + 1),
 resolvedRoutes.forEach((route) => {
-  if (!route.file || route.file.startsWith('routes'))
-    return;
-
-  const fix = route.id?.slice(0, 31);
-  route.file = fix + route.file;
+  if (!route.id || !route.file || route.file.startsWith('routes')) return;
+  route.file = `${route.id}.tsx`;
 });
 
 console.log(JSON.stringify(resolvedRoutes, null, 2));
